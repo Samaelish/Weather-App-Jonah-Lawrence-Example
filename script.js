23b8697e1883ac113133c957a033b98c4ade3537
Samaelish <semnd007@gmail.com>
2021-02-27 21:40:10 +0300

    fix case inflection rulang

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ let weather = {
       const { icon, description } = data.weather[0];
       const { temp, humidity } = data.main;
       const { speed } = data.wind;
-      document.querySelector(".city").innerText = "Погода в городе " + name;
+      document.querySelector(".city").innerText = "Погода " + name;
       document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + icon + ".png";
       document.querySelector(".description").innerText = description;
