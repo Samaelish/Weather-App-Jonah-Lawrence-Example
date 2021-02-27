253c9083ceae3314cf36496492c69f0e818274e1
Samaelish <semnd007@gmail.com>
2021-02-27 21:17:37 +0300

    ru lang

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ let weather = {
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +
           city +
-          "&units=metric&appid=" +
-          this.apiKey
+          "&units=metric&appid=" + 
+          this.apiKey +
+          "&lang=ru"
       )
         .then((response) => {
           if (!response.ok) {
@@ -21,15 +22,15 @@ let weather = {
       const { icon, description } = data.weather[0];
       const { temp, humidity } = data.main;
       const { speed } = data.wind;
-      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".city").innerText = "Погода в городе " + name;
       document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + icon + ".png";
       document.querySelector(".description").innerText = description;
       document.querySelector(".temp").innerText = temp + "°C";
       document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
+        "Влажность: " + humidity + "%";
       document.querySelector(".wind").innerText =
-        "Wind speed: " + speed + " km/h";
+        "Скорость ветра: " + speed + " км/ч";
       document.querySelector(".weather").classList.remove("loading");
       document.body.style.backgroundImage =
         "url('https://source.unsplash.com/1600x900/?" + name + "')";
